c9b8e49ad62f2d5a69175b85e859ceed812d4a59
hellen-adeniyi <hellenadeniyi29@gmail.com>
2024-02-28 16:56:23 -0600

    edited the auth.py, removed routes that didnt make sense

--- website/auth.py
+++ website/auth.py
@@ -6,15 +6,3 @@ auth = Blueprint('auth', __name__)  # created a blueprint
 @auth.route('/login')
 def login():
     return "Return the login page"
-
-@auth.route('/logout')
-def login():
-    return "Return the logout page"
-
-@auth.route('/sign in')
-def login():
-    return "Return the Signin page"
-
-@auth.route('/sign-up')
-def login():
-    return "Return the Sign up"
