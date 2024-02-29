46f28d9347953e9241e6ff175d4267e8b80a82fe
hellen-adeniyi <hellenadeniyi29@gmail.com>
2024-02-29 11:23:04 -0600

    Added a database

--- website/app.py
+++ website/app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy # for the databse
+from flask_login import UserMixin # flask login framework
 
+# create the flask instance
 app=Flask(__name__)
+
+#create a databse instance
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'game'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    phonenumber = db.Column(db.Integer(11), nullable=False)
+    dob = db.Column(db.String(9), nullable=False)
+    city = db.Column(db.String(20), nullable=False)
+    province = db.Column(db.String(20), nullable = False)
 
 @app.route('/')
 def game():
